fe105351cb2c28cdb7743fb24e927ff930a52f1a
thbspan <thbspan@163.com>
2020-10-02 12:46:24 +0800

    add apollo example

---
+++ apollo-example/src/main/java/com/test/apollo/example/ApolloExampleApplication.java
@@ -0,0 +1,50 @@
+package com.test.apollo.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class ApolloExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApolloExampleApplication.class);
+    }
+}
+
+@Component
+class OrderPropertiesCommandLineRunner implements CommandLineRunner {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private OrderProperties orderProperties;
+
+    @Override
+    public void run(String... args) {
+        logger.info("payTimeoutSeconds:" + orderProperties.getPayTimeoutSeconds());
+        logger.info("createFrequencySeconds:" + orderProperties.getCreateFrequencySeconds());
+    }
+}
+
+@Component
+class ValueCommandLineRunner implements CommandLineRunner {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Value(value = "${order.pay-timeout-seconds}")
+    private Integer payTimeoutSeconds;
+
+    @Value(value = "${order.create-frequency-seconds}")
+    private Integer createFrequencySeconds;
+
+    @Override
+    public void run(String... args) {
+        logger.info("payTimeoutSeconds:" + payTimeoutSeconds);
+        logger.info("createFrequencySeconds:" + createFrequencySeconds);
+    }
+}
