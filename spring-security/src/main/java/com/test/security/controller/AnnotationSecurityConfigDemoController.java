69bc3d8e79d7500b22f09ec00fc939057d222682
thbspan <thbspan@163.com>
2020-12-06 09:44:12 +0800

    add spring annotation config demo

---
+++ spring-security/src/main/java/com/test/security/controller/AnnotationSecurityConfigDemoController.java
@@ -0,0 +1,42 @@
+package com.test.security.controller;
+
+import javax.annotation.security.PermitAll;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/annotation")
+public class AnnotationSecurityConfigDemoController {
+    @PermitAll
+    @GetMapping("/echo")
+    public String demo() {
+        return "示例返回";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "我是首页";
+    }
+
+    /**
+     * {@code @Secured @RolesAllowed @PreAuthorize} 这三个注解都可以配置访问权限、
+     * 只是 {@code @EnableGlobalMethodSecurity}注解参数有些不同
+     * - @Secured 需要配置 securedEnabled = true
+     * - @RolesAllowed 需要配置 jsr250Enabled = true
+     * - @PreAuthorize 需要配置 prePostEnabled = true
+     */
+    @PreAuthorize("hasRole('admin')")
+    @GetMapping("/admin")
+    public String admin() {
+        return "我是管理员";
+    }
+
+    @PreAuthorize("hasRole('user')")
+    @GetMapping("/normal")
+    public String normal() {
+        return "我是普通用户";
+    }
+}
