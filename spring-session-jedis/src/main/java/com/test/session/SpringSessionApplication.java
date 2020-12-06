1c083427d61699c754a90339526aa392334b5b8e
thbspan <thbspan@163.com>
2020-12-06 21:05:52 +0800

    spring session add security

--- spring-session-jedis/src/main/java/com/test/session/SpringSessionApplication.java
+++ spring-session-jedis/src/main/java/com/test/session/SpringSessionApplication.java
@@ -2,10 +2,19 @@ package com.test.session;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.session.DefaultCookieSerializerCustomizer;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 public class SpringSessionApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringSessionApplication.class, args);
     }
+
+    @Bean
+    DefaultCookieSerializerCustomizer cookieSerializerCustomizer() {
+        return cookieSerializer -> cookieSerializer.setCookieName("ss");
+    }
+
+
 }
