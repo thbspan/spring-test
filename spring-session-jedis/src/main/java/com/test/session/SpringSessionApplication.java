75766816acb4ad06282c2da75cb95a4ac7a011a3
thbspan <thbspan@163.com>
2020-12-06 15:49:17 +0800

    add spring session demo

---
+++ spring-session-jedis/src/main/java/com/test/session/SpringSessionApplication.java
@@ -0,0 +1,11 @@
+package com.test.session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSessionApplication.class, args);
+    }
+}
