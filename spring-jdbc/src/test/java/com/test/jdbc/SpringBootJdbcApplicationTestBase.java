adcaf1b5d35c64a51446b714e241d4a148107674
thbspan <thbspan@163.com>
2020-12-26 11:55:39 +0800

    add spring boot test demo

---
+++ spring-jdbc/src/test/java/com/test/jdbc/SpringBootJdbcApplicationTestBase.java
@@ -0,0 +1,13 @@
+package com.test.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootJdbcApplicationTestBase {
+
+    @Test
+    public void testBase() {
+        System.out.println("test");
+    }
+}
