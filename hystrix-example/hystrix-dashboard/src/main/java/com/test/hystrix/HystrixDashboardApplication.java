3f3bfa5699ff639b968951d2de26ab098becb0a4
thbspan <thbspan@163.com>
2021-03-11 23:13:28 +0800

    add hystrix dashboard docs

--- hystrix-example/hystrix-dashboard/src/main/java/com/test/hystrix/HystrixDashboardApplication.java
+++ hystrix-example/hystrix-dashboard/src/main/java/com/test/hystrix/HystrixDashboardApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-// 开启hystrix dashboard功能
+/**
+ * 开启hystrix dashboard功能
+ * 不兼容Spring Boot 2.4.x版本，参考分支 netflix-hystrix
+ */
 @EnableHystrixDashboard
 @SpringBootApplication
 public class HystrixDashboardApplication {
