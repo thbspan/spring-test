72b8470e41e82cf859ebe80f2b52a5033e0295c4
thbspan <thbspan@163.com>
2021-03-12 14:16:22 +0800

    add Turbine example

--- hystrix-example/hystrix-dashboard/src/main/java/com/test/hystrix/HystrixDashboardApplication.java
+++ hystrix-example/hystrix-dashboard/src/main/java/com/test/hystrix/HystrixDashboardApplication.java
@@ -3,12 +3,15 @@ package com.test.hystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * 开启hystrix dashboard功能
  * 不兼容Spring Boot 2.4.x版本，参考分支 netflix-hystrix
  */
 @EnableHystrixDashboard
+// 开启 Turbine 功能
+@EnableTurbine
 @SpringBootApplication
 public class HystrixDashboardApplication {
     public static void main(String[] args) {
