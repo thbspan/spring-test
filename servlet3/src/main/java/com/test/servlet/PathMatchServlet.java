839422dd2fd0b2f4c9e12661f726774b3166e0a2
thbspan <thbspan@163.com>
2021-03-24 20:15:42 +0800

    add servlet docs

--- servlet3/src/main/java/com/test/servlet/PathMatchServlet.java
+++ servlet3/src/main/java/com/test/servlet/PathMatchServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * servlet url 路径匹配
+ * springboot 中的DispatcherServlet的匹配规则也是一样，不过它自动补上了*，所以能够匹配所有的子目录，
+ * 可以参考源码 DispatcherServletPath
  */
 //@WebServlet("/path*") // URL /path*
 //@WebServlet("/path/*") // URL /path/ /path /path/a /path/a/b
