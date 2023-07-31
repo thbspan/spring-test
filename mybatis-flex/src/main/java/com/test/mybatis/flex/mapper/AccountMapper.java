ce17bbbea7513d537f6344eb3a4c68996d4cbd91
thbspan <thbspan@163.com>
2023-07-31 18:27:15 +0800

    add mybatis plus & flex demo

---
+++ mybatis-flex/src/main/java/com/test/mybatis/flex/mapper/AccountMapper.java
@@ -0,0 +1,9 @@
+package com.test.mybatis.flex.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.test.mybatis.flex.entity.Account;
+
+public interface AccountMapper extends BaseMapper<Account> {
+
+
+}
---
+++ mybatis-plus/src/main/java/com/test/mybatis/plus/MybatisPlusApplication.java
@@ -0,0 +1,14 @@
+package com.test.mybatis.plus;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.test.mybatis.plus.mapper")
+public class MybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusApplication.class, args);
+    }
+}
